f6df324a72d6a8ca76029cee32490f9885cb8b8a
gaomingyang <gaomingyang@migu.cn>
2020-07-20 15:09:53 +0800

    小球围绕打球旋转的Demo.

--- FirstOpenGLDemo/FirstOpenGLDemo/main.cpp
+++ FirstOpenGLDemo/FirstOpenGLDemo/main.cpp
@@ -22,87 +22,163 @@
 #include <GL/glut.h>
 #endif
 
-GLFrustum           viewFrustum;
-GLShaderManager     shaderManager;
-GLTriangleBatch     torusBatch;
-GLGeometryTransform transformPipeline;
+GLShaderManager shaderManager;     //着色器管理器
+GLMatrixStack   modelViewMatrix;    //模型视图矩阵堆栈
+GLMatrixStack   projectionMatrix;   //投影矩阵堆栈
+GLFrustum       viewFrustum;        //视景体
+GLGeometryTransform transformPipline;   //几何图形变换管道
 
-//设置视口和投影矩阵
-void ChangeSize(int w,int h){
-    //防止除以零
-    if(h == 0 ){
-        h = 1;
-    }
-    //将视口设置为窗口尺寸
-    glViewport(0, 0, w, h);
+GLTriangleBatch     torusBatch;     //大球 （太阳）
+GLTriangleBatch     sphereBatch;    //小球 (卫星)
+GLBatch             floorBatch;     //地板
 
-    //设置透视投影
-    viewFrustum.SetPerspective(35.0f, float(w)/float(h), 1.0f, 1000.0f);
-}
+//角色帧  照相机角色帧
+GLFrame     cameraFrame;
 
-//召唤场景
-void RenderScene(void){
-    //清除屏幕缓冲，深度缓冲
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    //1.建立基于时间变化的动画
-    static CStopWatch rotTimer;
-    //当前时间 * 60s
-    float yRot = rotTimer.GetElapsedSeconds()*60.0f;
-    //2.矩阵变量
-    /*
-     mTranslate: 平移
 
-     mRotate: 旋转
-     mModelview: 模型视图
-     mModelViewProjection: 模型视图投影MVP
-     */
-    M3DMatrix44f  mTranslate,mRotate,mModelview,mModelViewprojection;
-
-    //创建一个 4x4 矩阵变量，将花托沿着z轴负方向移动2.5个长度单位
-    m3dTranslationMatrix44(mTranslate, 0.0f, 0.0f, -2.5f);
-    //创建一个 4x4 矩阵变量，将花托在Y轴上渲染yRot度，yRot根据经过时间设置动画帧率
-    m3dRotationMatrix44(mRotate, m3dDegToRad(yRot), 0.0f, 1.0f,0.0f);
-    //为mModerView 通过矩阵旋转矩阵，移动矩阵相乘，将结果添加到mModelView上
-    m3dMatrixMultiply44(mModelview, mTranslate, mRotate);
-    //将投影矩阵诚意模型视图矩阵，将变化结果通过矩阵长发应用到mModelViewProjection矩阵上
-    //注意顺序：投影 x 模型 ！= 模型 * 投影
-    m3dMatrixMultiply44(mModelViewprojection, viewFrustum.GetProjectionMatrix(), mModelview);
-    //绘图颜色
-    GLfloat vBlack[] = {0.0f,0.0f,0.0f,1.0f };
-
-    //通过平面着色器提交矩阵和颜色
-    shaderManager.UseStockShader(GLT_SHADER_FLAT,mModelViewprojection,vBlack);
-    //开始绘图
-    torusBatch.Draw();
-
-    //交换缓冲区
-    glutSwapBuffers();
-    //立即通知要刷新
-    glutPostRedisplay();
-}
+//**4. 添加附加随机球
+#define NUM_SPHERES 50
+GLFrame  spheres[NUM_SPHERES];
 
 void SetupRC(){
-    //1.设置背景色
-    glClearColor(0.8f, 0.8f, 0.8f, 1.0f);
+    //1.初始化
+    glClearColor(0, 0, 0, 1);
     shaderManager.InitializeStockShaders();
     glEnable(GL_DEPTH_TEST);
-    //2.形成一个球
-    gltMakeSphere(torusBatch, 0.4f, 10, 20);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // 地板数据（物体坐标系）
+    floorBatch.Begin(GL_LINES, 324);
+    for(GLfloat x = -20.0f; x<= 20.0f;x += 0.5){
+       floorBatch.Vertex3f(x, -0.55f, 20.0f);
+       floorBatch.Vertex3f(x, -0.55f, -20.0f);
+
+       floorBatch.Vertex3f(20.0f, -0.55f, x);
+       floorBatch.Vertex3f(-20.0f, -0.55f, x);
+    }
+    floorBatch.End();
+
+    // 设置一个球体(基于gltools模型)
+    gltMakeSphere(torusBatch, 0.4f, 40, 80);
+
+    //绘制小球
+    gltMakeSphere(sphereBatch, 0.1f, 13, 26);
+    //随机位置放置小球
+    for(int i=0;i<NUM_SPHERES;i++){
+        //y轴不变，x，z产生随机值
+        GLfloat x = ((GLfloat)((rand()%400) -200) * 0.1f);
+        GLfloat z = ((GLfloat)((rand()%400) -200) * 0.1f);
+
+        //在y方向 ，将球体设置为0.0的位置，这使得它们看起来是漂浮在眼镜的高度
+        //对spheres数组中的每一个顶点，设置顶点数据
+        spheres[i].SetOrigin(x,0.0f,z);
+    }
+}
+
+//进行调用以绘制场景
+void RenderScene(void){
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    //1.颜色（地板，打球颜色，小球颜色）
+    static GLfloat vFloorColor[] = {0.0f,1.0f,0.0f,1.0f};
+    static GLfloat vTorusColor[] = {1.0f,0.0f,0.0f,1.0f};
+    static GLfloat vSpereColor[] = {0.0f,0.0f,1.0f,1.0f};
+
+    //2.动画
+    static CStopWatch rotTimer;
+    float yRot = rotTimer.GetElapsedSeconds()*60.0f;
+
+    modelViewMatrix.PushMatrix();
+    //3. 压栈
+    M3DMatrix44f mCamera;
+    cameraFrame.GetCameraMatrix(mCamera);
+    modelViewMatrix.PushMatrix(mCamera);
+
+    //4.地面绘制
+    shaderManager.UseStockShader(GLT_SHADER_FLAT,transformPipline.GetModelViewProjectionMatrix(),vFloorColor);
+    floorBatch.Draw();
+
+    //5.设置点光源位置
+    M3DVector4f vLightPos = {0,10,5,1};
+
+    //6.使得整个打球往里面平移3.0
+    modelViewMatrix.Translate(0.0f, 0.0f, -3.0f);
+
+    //7.大球
+    modelViewMatrix.PushMatrix();
+    modelViewMatrix.Rotate(yRot, 0, 1, 0);
+    shaderManager.UseStockShader(GLT_SHADER_POINT_LIGHT_DIFF,transformPipline.GetModelViewMatrix(),transformPipline.GetProjectionMatrix(),vLightPos,vTorusColor);
+    torusBatch.Draw();
+    modelViewMatrix.PopMatrix();
+
+    //8.小球
+    for(int i=0;i<NUM_SPHERES;i++ ){
+        modelViewMatrix.PushMatrix();
+        modelViewMatrix.MultMatrix(spheres[i]);
+        shaderManager.UseStockShader(GLT_SHADER_POINT_LIGHT_DIFF,transformPipline.GetModelViewMatrix(),transformPipline.GetProjectionMatrix(),vLightPos,vSpereColor);
+        sphereBatch.Draw();
+        modelViewMatrix.PopMatrix();
+    }
+
+    //9.让一个小球围绕着打球公转;
+    modelViewMatrix.Rotate(yRot * -2.0f, 0, 1, 0);
+    modelViewMatrix.Translate(0.8f, 0.0f, 0.0f);
+    shaderManager.UseStockShader(GLT_SHADER_POINT_LIGHT_DIFF,transformPipline.GetModelViewMatrix(),transformPipline.GetProjectionMatrix(),vLightPos,vSpereColor);
+    sphereBatch.Draw();
+
+    modelViewMatrix.PopMatrix();
+    modelViewMatrix.PopMatrix();
+    glutSwapBuffers();
+    glutPostRedisplay();
+
+}
+
+//屏幕更改大小或已初始化
+void ChangeSize(int width ,int height){
+    //1.设置视口
+    glViewport(0, 0, width, height);
+
+    //2.创建投影矩阵
+    viewFrustum.SetPerspective(35.0f, float(width)/float(height), 1.0f, 100.0f);
+    projectionMatrix.LoadMatrix(viewFrustum.GetProjectionMatrix());
+
+    //3.变换管道设置2个矩阵堆栈（管理）
+    transformPipline.SetMatrixStacks(modelViewMatrix, projectionMatrix);
+}
+
+void SpeacialKeys(int key,int x,int y){
+    float linear = 0.1f;
+    float anguar = float(m3dDegToRad(5.0f));
+
+    if(key == GLUT_KEY_UP){
+        cameraFrame.MoveForward(linear);
+    }
+    if(key == GLUT_KEY_DOWN){
+        cameraFrame.MoveForward(-linear);
+    }
+
+    if(key == GLUT_KEY_LEFT){
+        cameraFrame.RotateWorld(anguar, 0, 1, 0);
+    }
+    if(key == GLUT_KEY_RIGHT){
+        cameraFrame.RotateWorld(-anguar, 0, 1, 0);
+    }
 }
 
 int main(int argc,char* argv[]){
     gltSetWorkingDirectory(argv[0]);
 
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_STENCIL);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(800, 600);
-    glutCreateWindow("ModelViewProjection Example");
+
+    glutCreateWindow("OpenGL sphereWorld");
+
     glutReshapeFunc(ChangeSize);
     glutDisplayFunc(RenderScene);
+    glutSpecialFunc(SpeacialKeys);
 
-    GLenum err = glewInit();
-    if(GLEW_OK != err ){
+    GLenum  err = glewInit();
+    if(GLEW_OK != err){
         fprintf(stderr, "GLEW Error:%s\n", glewGetErrorString(err));
         return 1;
     }
@@ -111,7 +187,6 @@ int main(int argc,char* argv[]){
     glutMainLoop();
     return 0;
 }
-
 
 
 
